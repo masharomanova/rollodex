146c37480986562647b53212d2ccec856e9538fb
masharomanova <mariavromanova@yahoo.com>
2019-06-05 21:05:20 -0500

    still need to be able to hide details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function UserCard (props) {
+
+  let emailInfo =  (
+    <div>
+    <span>{props.user.email}</span>
     </div>
   );
+
+  let buttonText
+
+  if (props.isHidden) {
+    buttonText = "Show Details";
+  } else {
+    buttonText = "Hide Details";
+  }
+
+  return (
+  <div style={{ marginBottom: '40px' }}>
+      <img src={props.user.picture.large}></img>
+    <div>
+      <span>{props.user.name.first}</span>
+      {' '}
+      <span>{props.user.name.last}</span>
+    </div>
+    <div>
+      {props.isHidden ? null : emailInfo}
+    </div>
+    <button onClick={props.onClick}>{buttonText}</button>
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isHidden: true
+    };
+}
+
+componentDidMount() {
+  fetch('https://randomuser.me/api?results=25')
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    this.setState({
+      results: json.results
+    })
+  })
+}
+
+onClick = (event) => {
+  this.setState({
+    isHidden: false
+  });
+}
+
+
+render() {
+  return (
+    <div className="App">
+      {this.state.results.map((user, index) => 
+        <UserCard 
+          key={index} 
+          user={user} 
+          onClick={this.onClick} 
+          isHidden={this.state.isHidden} />
+      )}
+    </div>
+  );
+}
 }
 
 export default App;
